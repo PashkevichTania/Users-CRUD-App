28179f838bc88c329c4ed95abcf1fc5868ff0115
PashkevichTania <pashkevichtania2004@gmail.com>
2021-10-21 22:56:51 +0300

    refactor: add project files

--- users-crud-app/src/components/App.jsx
+++ users-crud-app/src/components/App.jsx
@@ -1,12 +1,33 @@
 import Home from "pages/home/home";
+import {getCookie} from "cookieUtils/cookieUtils";
+import {useQuery} from "react-query";
+import {login} from "services/auth";
+import {getAllUsersPaginated} from "services/apiRequests";
+import {useEffect, useState} from "react";
 
 
 function App() {
 
-    return (
+    const [isAuth, setAuth] = useState(false);
 
+    useEffect(async ()=>{
+        // console.group('cookie: ')
+        // console.log(document.cookie)
+        // console.log('cookie user: ', getCookie('X-AUTH-USER-CRUD'))
+        // console.groupEnd()
+
+
+        const res = await getAllUsersPaginated(1,1)
+        setAuth(res.responseStatus.isAuth)
+        console.log('res:s ',res)
+
+    },[])
+
+
+
+    return (
         <div className="App">
-            <Home/>
+            {isAuth? <Home/>: null}
         </div>
 
     );
